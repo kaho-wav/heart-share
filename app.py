755df4eada4a839e9b61855d586d87d1c37f6072
かほ <te24hayashi@g.kumamoto-nct.ac.jp>
2026-05-28 20:57:32 +0900

    add requirements

--- app.py
+++ app.py
@@ -1,19 +1,59 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from flask import request
+from flask import redirect
+from flask import session
+
 import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
+app.secret_key = "secret123"
+
+PASSWORD = "love"
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        password = request.form["password"]
+
+        if password == PASSWORD:
+
+            session["login"] = True
+
+            return redirect("/heart-view")
+
+    return render_template("login.html")
+
+
+@app.route("/heart-view")
+def heart_view():
+
+    if not session.get("login"):
+
+        return redirect("/")
+
     return render_template("index.html")
+
 
 @app.route("/heart")
 def heart():
+
+    if not session.get("login"):
+
+        return jsonify({
+            "error": "login required"
+        })
+
     bpm = random.randint(70, 90)
 
     return jsonify({
         "bpm": bpm
     })
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
